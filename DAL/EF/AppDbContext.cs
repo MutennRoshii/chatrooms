80d1ed1aef30c0e06d63ac51021ad0155fc3af54
Abdelkarim Lamrini <abdelkarim.lamrini@student.kdg.be>
2023-02-26 02:30:23 +0100

    Before changing UserManager<>

--- DAL/EF/AppDbContext.cs
+++ DAL/EF/AppDbContext.cs
@@ -1,19 +1,41 @@
-﻿using ChatApp.BL.Domain;
+﻿using System.Diagnostics;
+using ChatApp.BL.Domain;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ChatApp.DAL.EF;
 
 public class AppDbContext : IdentityDbContext<User>
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Membership> Memberships { get; set; }
-    public DbSet<Server> Servers { get; set; }
-    public DbSet<Channel> Channels { get; set; }
-    public DbSet<Message> Messages { get; set; }
+    public new DbSet<User>? Users { get; set; }
+    public DbSet<Membership>? Memberships { get; set; }
+    public DbSet<Server>? Servers { get; set; }
+    public DbSet<Channel>? Channels { get; set; }
+    public DbSet<Message>? Messages { get; set; }
 
-    public AppDbContext()
+    public AppDbContext(DbContextOptions options) : base(options)
     {
         AppDbInitializer.Initialize(this, true);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(message => Debug.WriteLine(message), LogLevel.Information);
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Membership>()
+            .HasOne<User>()
+            .WithMany(user => user.Memberships)
+            .IsRequired();
+        builder.Entity<Membership>()
+            .HasOne<Server>()
+            .WithMany(server => server.Members)
+            .IsRequired();
     }
 }
--- Domain/Membership.cs
+++ Domain/Membership.cs
@@ -4,8 +4,9 @@ namespace ChatApp.BL.Domain;
 
 public class Membership
 {
-    [Key] public User User { get; set; }
-    [Key] public Server Server { get; set; }
+    [Key] public int Id { get; set; }
+    [Required] public Server Server { get; set; }
+    [Required] public User User { get; set; }
     [StringLength(32, MinimumLength = 3)] public string Username { get; set; }
     public ICollection<Message> Messages { get; set; }
     public DateTime JoinedTime { get; set; }
